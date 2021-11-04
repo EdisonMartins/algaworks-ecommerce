6d17a92995ff903e5e2712c60848c244ea7c4a62
Edison Martins <martinsegmj@gmail.com>
2021-11-03 23:03:14 -0300

    4.9. Mapeamento relacionamentos um-para-um com @OneToOne

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -40,6 +40,8 @@ public class Pedido {
     @Enumerated(EnumType.STRING)
     private StatusPedido status;
 
+    @OneToOne(mappedBy = "pedido")
+    private PagamentoCartao pagamento;
 
     @Embedded
     private EnderecoEntregaPedido enderecoEntrega;
